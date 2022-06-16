14e5fe023898d3e69ed109a30acbc43f1589283f
zX3no <dr_xeno@hotmail.com.au>
2022-06-16 15:17:52 +0930

    program class

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,64 @@
 use std::fs::File;
 use std::io::Read;
+use std::path::Path;
 
 use glow::*;
 use glutin::event::{Event, WindowEvent};
 use glutin::event_loop::ControlFlow;
 
-enum Shader {
-    Vertex,
-    Fragment,
+struct Program {
+    native_program: NativeProgram,
 }
-impl Shader {
-    pub fn new(path: &str) -> String {
+
+impl Program {
+    pub fn new(
+        gl: &Context,
+        vertex_path: impl AsRef<Path>,
+        fragment_path: impl AsRef<Path>,
+    ) -> Self {
+        unsafe {
+            let v_source = Self::open(vertex_path);
+            let f_source = Self::open(fragment_path);
+            let program = gl.create_program().unwrap();
+
+            //Vertex shader
+            let v = gl.create_shader(glow::VERTEX_SHADER).unwrap();
+            let error = gl.get_shader_info_log(v);
+            if !error.is_empty() {
+                panic!("{}", error);
+            }
+            gl.shader_source(v, &v_source);
+            gl.compile_shader(v);
+            gl.attach_shader(program, v);
+
+            //Fragment shader
+            let f = gl.create_shader(glow::FRAGMENT_SHADER).unwrap();
+            let error = gl.get_shader_info_log(f);
+            if !error.is_empty() {
+                panic!("{}", error);
+            }
+            gl.shader_source(f, &f_source);
+            gl.compile_shader(f);
+            gl.attach_shader(program, f);
+
+            //Link program
+            gl.link_program(program);
+            if !gl.get_program_link_status(program) {
+                panic!("{}", gl.get_program_info_log(program));
+            }
+
+            gl.use_program(Some(program));
+
+            //Cleanup
+            gl.delete_shader(v);
+            gl.delete_shader(f);
+
+            Self {
+                native_program: program,
+            }
+        }
+    }
+    pub fn open(path: impl AsRef<Path>) -> String {
         let mut file = File::open(path).unwrap();
         let mut buf = String::new();
         file.read_to_string(&mut buf).unwrap();
@@ -44,41 +92,10 @@ fn main() {
 
         let gl = glow::Context::from_loader_function(|s| window.get_proc_address(s) as *const _);
 
-        let program = gl.create_program().expect("Cannot create program");
-        let create_shader = |source, s_type| -> NativeShader {
-            let shader = match s_type {
-                Shader::Vertex => gl.create_shader(glow::VERTEX_SHADER),
-                Shader::Fragment => gl.create_shader(glow::FRAGMENT_SHADER),
-            }
-            .unwrap();
-            let error = gl.get_shader_info_log(shader);
-            if !error.is_empty() {
-                panic!("{}", error);
-            }
-            gl.shader_source(shader, source);
-            gl.compile_shader(shader);
-            gl.attach_shader(program, shader);
-            shader
-        };
-
-        let source = Shader::new("src/vertex.glsl");
-        let v = create_shader(&source, Shader::Vertex);
-
-        let source = Shader::new("src/fragment.glsl");
-        let f = create_shader(&source, Shader::Fragment);
-
-        gl.link_program(program);
-        if !gl.get_program_link_status(program) {
-            panic!("{}", gl.get_program_info_log(program));
-        }
-
-        gl.use_program(Some(program));
-
-        gl.delete_shader(v);
-        gl.delete_shader(f);
+        let program = Program::new(&gl, "src/vertex.glsl", "src/fragment.glsl");
 
         {
-            let vertices: &[f32] = &[
+            let square: &[f32] = &[
                 0.5, -0.5, 0.0, //Position
                 1.0, 0.0, 0.0, //Color
                 -0.5, -0.5, 0.0, //Position
@@ -93,7 +110,7 @@ fn main() {
             gl.bind_vertex_array(Some(vao));
 
             gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
-            gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, buffer(vertices), glow::STATIC_DRAW);
+            gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, buffer(square), glow::STATIC_DRAW);
 
             //Position
             gl.vertex_attrib_pointer_f32(0, 3, glow::FLOAT, false, 6 * 4, 0);
@@ -103,6 +120,8 @@ fn main() {
             gl.vertex_attrib_pointer_f32(1, 3, glow::FLOAT, false, 6 * 4, 3 * 4);
             gl.enable_vertex_attrib_array(1);
         }
+
+        gl.clear_color(0.1, 0.2, 0.3, 1.0);
 
         event_loop.run(move |event, _, control_flow| {
             *control_flow = ControlFlow::Poll;
@@ -125,7 +144,7 @@ fn main() {
                         window.resize(*physical_size);
                     }
                     WindowEvent::CloseRequested => {
-                        gl.delete_program(program);
+                        gl.delete_program(program.native_program);
                         // gl.delete_vertex_array(vertex_array);
                         *control_flow = ControlFlow::Exit
                     }
